ba68715e2cb6fcc1f8210d168f74b9198b547472
mesafintminwagaw23@gmail.com <mesafintminwagaw23@gmail.com>
2023-04-02 16:45:38 +0300

    deploying netflix app on firebase

---
+++ netflix.com-clone/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import Banner from './Banner/Banner';
+import Row from './Row/Row';
+import requests from './requests'
+import Nav from './Nav/Nav';
+import Footer from './Footer/Footer';
+
+function App() {
+
+  return (
+    <div className="App">
+			<Nav />
+			<Banner />
+      		<Row title="NETFLIX ORIGINALS"
+				fetchUrl={requests.fetchNetflixOriginals}
+				isLargeRow={true}/>
+            <Row title="Trending Now" fetchUrl={requests.fetchTrending} /> 
+		    <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies} />
+			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row
+				title="Documentaries Movies"
+				fetchUrl={requests.fetchDocumentaries}
+			/>
+			<Footer />
+    </div>
+  );
+}
+
+export default App;
